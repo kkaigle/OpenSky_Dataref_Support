eefd33dd7df7d2bae0cce987e9bcb50630f1c883
kkaigle <83706649+kkaigle@users.noreply.github.com>
2023-12-14 12:02:45 -0500

    Update FF_A320_engines.lua
    added multi engine support

--- OpenSky/FF_A320_engines.lua
+++ OpenSky/FF_A320_engines.lua
@@ -1,12 +1,18 @@
-320_Eng_1		= find_dataref("sim/flightmodel/engine/ENGN_FF_")
+320_Eng_1		= find_dataref("sim/flightmodel/engine/ENGN_FF_[0]")
 Xpl_Eng_1		= find_dataref("sim/flightmodel/engine/ENGN_running[0]")
-
+320_Eng_2		= find_dataref("sim/flightmodel/engine/ENGN_FF_[1]")
+Xpl_Eng_2		= find_dataref("sim/flightmodel/engine/ENGN_running[1]")
 
 function engines()
 	if 320_Eng_1 > 0 then
 		Xpl_Eng_1 = 1
 	else
 		Xpl_Eng_1 = 0
+	end
+	if 320_Eng_2 > 0 then
+		Xpl_Eng_2 = 1
+	else
+		Xpl_Eng_2 = 0
 	end
 
 end
