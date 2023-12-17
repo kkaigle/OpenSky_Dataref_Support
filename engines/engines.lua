53da6baa60470c2ead7bfdad94d65a0fd8d21a10
kkaigle <83706649+kkaigle@users.noreply.github.com>
2023-12-16 22:17:41 -0500

    Update engines.lua
    Updated to support 4 engine aircraft

--- engines/engines.lua
+++ engines/engines.lua
@@ -1,5 +1,11 @@
 Eng_1		= find_dataref("sim/flightmodel/engine/ENGN_FF_[0]")
 Xpl_Eng_1		= find_dataref("sim/flightmodel/engine/ENGN_running[0]")
+Eng_2		= find_dataref("sim/flightmodel/engine/ENGN_FF_[0]")
+Xpl_Eng_2		= find_dataref("sim/flightmodel/engine/ENGN_running[0]")
+Eng_3		= find_dataref("sim/flightmodel/engine/ENGN_FF_[0]")
+Xpl_Eng_3		= find_dataref("sim/flightmodel/engine/ENGN_running[0]")
+Eng_4		= find_dataref("sim/flightmodel/engine/ENGN_FF_[0]")
+Xpl_Eng_4		= find_dataref("sim/flightmodel/engine/ENGN_running[0]")
 
 
 function engines()
@@ -7,6 +13,21 @@ function engines()
 		Xpl_Eng_1 = 1
 	else
 		Xpl_Eng_1 = 0
+	end
+    if Eng_2 > .001 then
+		Xpl_Eng_2 = 1
+	else
+		Xpl_Eng_2 = 0
+	end
+    if Eng_3 > .001 then
+		Xpl_Eng_3 = 1
+	else
+		Xpl_Eng_3 = 0
+	end
+    if Eng_4 > .001 then
+		Xpl_Eng_4 = 1
+	else
+		Xpl_Eng_4 = 0
 	end
 
 end
